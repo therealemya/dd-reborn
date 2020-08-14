4b134d226a28db0fcb83de53e9f8da76406e151b
Eric <tlasecaeric@gmail.com>
2020-08-14 15:13:29 -0400

    Fixed css, size, responsiveness, color,

--- src/components/UserContainer/UserHome.js
+++ src/components/UserContainer/UserHome.js
@@ -5,23 +5,29 @@ import './UserHome.css';
 
 export default class UserHome extends Component{
     render() {
+        
         return(
 
             <div className="user-home">
                  
                 <div className="user-container">
-                    
+                    <center>
                 <div className="userLogo">
                     <img src={logo} alt="logo" />
+                  
                     </div>
+                    </center>
+                    <center>
                     <hr/>
                     <div className ="Border">
                     <div className ="MyPro">
 
                     <h5>My Profile</h5>
+                
                     </div>
                     </div>
- 
+                    </center>
+                    <center>
                     <hr/> 
                             <div className = "MyDon">
                     <h5><Link to='/user/mydonations' className="user-link">My Donations</Link></h5>
@@ -30,16 +36,22 @@ export default class UserHome extends Component{
                              <div className = "MyPay">
                     <h5><Link to="user/payments" className="user-link">Payment</Link></h5>
                     </div>
-
+                    </center>
+                    <center>
                     <hr/>
                             <div className = "MySet">
                     <h5>Settings</h5>
                       
                     <br/>
                     <br/>
+                    <br/> 
+                    <br/>
+                    <br/> 
                     <Link to="/user/create" id="user-home-link"><button>Create A Donation</button></Link>
                     </div>
+                    </center>
                 </div>
+                
 
             </div>
 
