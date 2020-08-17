b660d0d343ebdda202848f819dc19440149d2963
Eric <tlasecaeric@gmail.com>
2020-08-17 13:38:48 -0400

    Media feed added

--- src/components/SocialFeedBodyComponent/FeedBody.js
+++ src/components/SocialFeedBodyComponent/FeedBody.js
@@ -2,10 +2,12 @@ import React, {Component} from "react";
 import "./FeedBody.css";
 import Posts from '../postsComponent/Posts';
 
+
 export default class FeedBody extends Component{
     render() {
         return (
             <div className= "body-section">
+               
       <Posts/>
       </div>
             );
